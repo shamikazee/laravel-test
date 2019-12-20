320d20c5b13f0e13e7077303d2a27a5f3b1fc6f5
unknown <kos140.resultat@gmail.com>
2019-12-20 21:26:34 +0100

    3rd 4th and 5th questions

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -32,16 +32,6 @@ class CategoryController extends Controller
         }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -33,18 +33,6 @@ class CourseController extends Controller
             return 'course Created';
         }
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
     /**
      * Display the specified resource.
      *
